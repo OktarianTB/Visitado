71cfe4dcd647722e793dc10e3edebf62dfb36db1
OktarianTB <oktotb@gmail.com>
2021-03-27 16:17:59 +0800

    Modified Login & Register Pages with new logo

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,62 +1,76 @@
 import React from "react";
 import styles from "./Login.module.css";
-import {
-  Button,
-  CssBaseline,
-  TextField,
-  Link,
-  Typography,
-  Container,
-} from "@material-ui/core/";
+import { Button, TextField, Link, Typography, Grid } from "@material-ui/core/";
 
 const Login = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={styles.paper}>
+    <Grid
+      container
+      spacing={3}
+      direction="row"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "95vh" }}
+    >
+      <Grid xs={6}>
         <img src="koala.png" alt="koala logo" className={styles.logo} />
-        <Typography component="h1" variant="h5">
-          Log In
-        </Typography>
-        <form className={styles.form}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            disableElevation
-            variant="contained"
-            color="primary"
-            className={styles.submit}
+      </Grid>
+
+      <Grid xs={6}>
+        <div className={styles.login}>
+          <Typography
+            component="h1"
+            variant="h5"
+            style={{ width: "50%", textAlign: "center" }}
           >
-            Sign In
-          </Button>
-          <Link href="/register" variant="body2">
-            {"Don't have an account?"}
-          </Link>
-        </form>
-      </div>
-    </Container>
+            Log In
+          </Typography>
+          <LoginForm />
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+const LoginForm = () => {
+  return (
+    <form className={styles.form}>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <Button
+        type="submit"
+        fullWidth
+        disableElevation
+        variant="contained"
+        color="primary"
+        className={styles.submit}
+      >
+        Sign In
+      </Button>
+      <Link href="/register" variant="body2">
+        {"Don't have an account?"}
+      </Link>
+    </form>
   );
 };
 
--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -14,75 +14,97 @@ import {
 
 const Register = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={styles.paper}>
+    <Grid
+      container
+      spacing={3}
+      direction="row"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "95vh" }}
+    >
+      <Grid xs={6}>
         <img src="koala.png" alt="koala logo" className={styles.logo} />
-        <Typography component="h1" variant="h5">
-          Register
-        </Typography>
-        <form className={styles.form}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label={
-                  <Link href="/terms.txt">
-                    I agree to the terms and conditions.
-                  </Link>
-                }
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={styles.submit}
+      </Grid>
+
+      <Grid xs={6}>
+        <div className={styles.register}>
+          <Typography
+            component="h1"
+            variant="h5"
+            style={{ width: "50%", textAlign: "center", marginBottom: 20 }}
           >
             Register
-          </Button>
-          <Link href="/login" variant="body2">
-            {"Already have an account?"}
-          </Link>
-        </form>
-      </div>
-    </Container>
+          </Typography>
+          <RegisterForm />
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+const RegisterForm = () => {
+  return (
+    <form className={styles.form}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox value="allowExtraEmails" color="primary" />}
+            label={
+              <Link href="/terms.txt">
+                I agree to the terms and conditions.
+              </Link>
+            }
+          />
+        </Grid>
+      </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={styles.submit}
+      >
+        Register
+      </Button>
+      <Link href="/login" variant="body2">
+        {"Already have an account?"}
+      </Link>
+    </form>
   );
 };
 
