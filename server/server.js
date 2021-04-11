421a7a4f0bdcdc438069548651c72cdb98d2c547
OktarianTB <oktotb@gmail.com>
2021-04-11 23:57:06 +0800

    CORS fix

--- server/server.js
+++ server/server.js
@@ -59,6 +59,17 @@ app.use(function (req, res, next) {
   next(createError(404, "This endpoint does not exist."));
 });*/
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 // error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).json({
